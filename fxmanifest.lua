926c98e8ce91c2cec838b55c684c7f5c5d8e9365
BDN_fr <98167323+BDN-fr@users.noreply.github.com>
2024-06-10 04:31:40 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+games {'gta5'}
+fx_version 'cerulean'
+server_only 'yes'
+
+author 'BDN_fr'
+description 'A script that allows you to have one hour on your correct time zone without having to change/do calculations to have the right time in your scripts.'
+version '1.0'
+
+server_scripts {'server/*.lua','config.lua'}
