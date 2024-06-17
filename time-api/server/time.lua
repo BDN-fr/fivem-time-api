4c7ad99516c4dadb6f8f827900c571ea9df739bb
BDN_fr <98167323+BDN-fr@users.noreply.github.com>
2024-06-17 12:22:39 +0200

    Update time.lua

--- time-api/server/time.lua
+++ time-api/server/time.lua
@@ -1,9 +1,20 @@
-function GetTime(args)
-    local time
-    PerformHttpRequest(('http://worldtimeapi.org/api/timezone/%s'):format(args.TimeZone or Config.TimeZone),function (status, data, headers, errorData)
-        if not data then print('API ERROR : '..status) return end
-        time = json.decode(data)
-    end)
-    return time
+TimeDiff = 0
+
+function GetTime()
+    return os.time() + TimeDiff
 end
 exports('GetTime',GetTime)
+
+function Init()
+    PerformHttpRequest(('http://worldtimeapi.org/api/timezone/%s'):format(Config.TimeZone),function (status, data, headers, errorData)
+        if not data then
+            print('API ERROR : '..status)
+            Citizen.CreateThread(function()
+                Wait(5 * 60000) -- Wait 5 minutes
+                Init() -- Retry
+            end)
+            return
+        end
+        TimeDiff = os.time() - json.decode(data).unixtime
+    end)
+end
